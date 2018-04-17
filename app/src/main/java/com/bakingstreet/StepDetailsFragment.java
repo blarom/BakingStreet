59cdbd2b980ffeb59d76e91efd885f88c441b1e6
Bar Lr <bar.larom@gmail.com>
2018-04-17 23:46:53 +0300

    Added Exoplayer.

--- app/src/main/java/com/bakingstreet/StepDetailsFragment.java
+++ app/src/main/java/com/bakingstreet/StepDetailsFragment.java
@@ -2,6 +2,8 @@ package com.bakingstreet;
 
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,11 +12,24 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bakingstreet.data.Constants;
 import com.bakingstreet.data.Recipe;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.net.URL;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,10 +43,11 @@ public class StepDetailsFragment extends Fragment {
     private String mThumbnailUrl;
     private String mDescription;
     private String mShortDescription;
-    @BindView(R.id.step_image) ImageView mStepImageView;
+    @BindView(R.id.step_video) SimpleExoPlayerView mStepVideoView;
     @BindView(R.id.step_description) TextView mStepDescriptionTextView;
     @BindView(R.id.step_short_description) TextView mStepShortDescriptionTextView;
     NavigationButtonClickHandler mOnNavigationButtonClickHandler;
+    private SimpleExoPlayer mExoPlayer;
 
     public StepDetailsFragment() {
         // Required empty public constructor
@@ -56,6 +72,11 @@ public class StepDetailsFragment extends Fragment {
     public void onAttach(Context context) {
         super.onAttach(context);
         mOnNavigationButtonClickHandler = (NavigationButtonClickHandler) context;
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        releasePlayer();
     }
 
     private void setupBottomNavigation() {
@@ -92,6 +113,39 @@ public class StepDetailsFragment extends Fragment {
     private void setupLayoutValues() {
         if (mDescription!=null) mStepDescriptionTextView.setText(mDescription);
         if (mShortDescription!=null) mStepShortDescriptionTextView.setText(mShortDescription);
+        if (mStepVideoView != null) {
+            mStepVideoView.setDefaultArtwork(BitmapFactory.decodeResource(getResources(), R.drawable.ic_missing_image));
+
+            initializeVideoPlayer(mVideoUrl);
+        }
+    }
+    private void initializeVideoPlayer(String videoUrl) {
+
+        if (getContext()==null) return;
+
+        // Setup ExoPlayer
+        TrackSelector trackSelector = new DefaultTrackSelector();
+        LoadControl loadControl = new DefaultLoadControl();
+        mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+        mStepVideoView.setPlayer(mExoPlayer);
+
+        // Setup MediaSource
+        String userAgent = Util.getUserAgent(getContext(), "ClassicalMusicQuiz");
+        MediaSource mediaSource = new ExtractorMediaSource(
+                Uri.parse(videoUrl),
+                new DefaultDataSourceFactory(getContext(), userAgent),
+                new DefaultExtractorsFactory(),
+                null,
+                null);
+        mExoPlayer.prepare(mediaSource);
+        mExoPlayer.setPlayWhenReady(true);
+    }
+    private void releasePlayer() {
+        if (mExoPlayer != null) {
+            mExoPlayer.stop();
+            mExoPlayer.release();
+            mExoPlayer = null;
+        }
     }
 
     //Functional methods
